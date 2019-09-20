ad0ee6830c9cf18d7582d2847bc02f016623fa3b
kevinkii <kevinmtaing@Monireangseys-MacBook-Pro.local>
2019-09-20 10:24:40 -0700

    update info

--- dataquest1-summary.py
+++ dataquest1-summary.py
@@ -1,14 +1,14 @@
-# %% [markdown]
-#  ##Python Basic Code
+#%% [markdown]
+# ##Python Basic Code
 
-# %%
+#%%
 a_value = 15
 a_result = (27-7)*17
 print(a_value)
 print(a_value+12)
 print(a_result + a_value)
 
-# %% [markdown]
+#%% [markdown]
 # | Snytax Shortcut | Example Code  | Output |
 # |-----------------|---------------|--------|
 # | +=              | x = 4, x += 2 | 6      |
@@ -17,62 +17,84 @@ print(a_result + a_value)
 # | /=              | x = 4, x /= 2 | 2      |
 # | **=             | x = 4, x**=2  | 16     |
 
-# %%
-# the `int()` command rounded `4.6` down to `4`
-# the `round()` command rounded `4.6` up to `5`
-# the string `"Facebook"` or `'Facebook'` is the `str` type
-# the integer `2` has the `int` type, and the deciamal number `8.5` has the `float` type
+#%%
+float(10)
+int(4.6) #round down
+round(4.6) #round up
+type('Facebook') #string
+print("a" * 5) #repeat "a" 5 five
+print("a" * 0) #no output
+print("a" * -1) #no output
 
-# %%
+#%%
 motto = 'Facebook\'s old motto was "move fast and break things."'
 print(motto)
 
-# %% [markdown]
-# In python, we have two indexing system for lists:
-# * **Positive indexing**: the frist element has he index number 0, the second element has the index number 1, and so on.
-# * **Negative indexing**: the last element has the index number -1, the second to last element has the index number -2, and so on.
+#%% [markdown]
+# ## List in Python
 
+#%%
+a_list = [] #creating an empty list
+row1 = ["Facebook", 0.0, "USA", 2857673, 3.5] #creating a list
+row2 = ["Instagram", 0.0, "USA", 2567673, 4.5] #creating a list
+data = [row1, row2] #creating a list of lists
+first_row = data[0]
+first_element_in_first_row = data[0][0] #`first_row[0]`
+last_element_in_first_row = data[0][-1] #`first_row[-1]`
 
 # %% [markdown]
 # ## Data Slicing
-# `x[start:stop:step]`
-#
-# `x[:5]` first five element
-#
-# `x[5:]` element after index 5
-#
-# `x[1:5]` from index 1 to index 4
-# 
-# `x[::2]` every other element
-#
-# `x[1::2]` every other element, starting from index 1
-#
-# `x[::-1]` all element, reverse
-#
-# `x[5::-2]` reverse every other element from index 5
 
-# %% [markdown]
-# data_set[0][1] = the second element of the fisrt row in the data_set
-#
-# create a empty list = `a_list = []`
+#%%
+import numpy as np
+x = np.arange(10)
+x # it is the same as the positive index
+
+x[0] #the fist element
+x[-1] #the last element
+x[:5] #first five element
+x[5:] #element after index 5
+x[1:5] #from index 1 to index 4
+x[::2] #every other element
+x[1::2] #every other element, starting from index 1
+x[::-1] #all element, reverse
+x[5::-2] #reverse every other element from index 5
 
 # %% [markdown]
 # ## How to read file using "reader"
 #
 # `from csv import reader`
 #
-# `opened_file = open('AppleStore.csv`
+# `opened_file = open('AppleStore.csv)`
 #
 # `read_file = reader()`
 #
 # `data_set = list(read_file)`
 
-# %% [markdown]
+#%% [markdown]
 # ## `For` loop in Python
-#
-# `for each_list in data_set` mean read every row in the data_set
-#
-# Once we create a list, we can add (or **append**) values to it using the `append()` command. it has a pattern of `list_name.append()`
+# `for` loop will iterate every element one by one in a list or every row row by row in a data.
+
+#%%
+rating = [3,5,1,2]
+for element in rating:
+    print(element)
+
+#%%
+a_list = [1,3,5]
+for value in a_list:
+    print(value)
+    print(value -1)
+
+a_sum = 0
+for value in a_list:
+    a_sum += value
+    print(a_sum)
+
+#%%
+list1 = [1, 2]
+list1.append(3) #add integer 3 to list1
+print(list1)
 
 # %% [markdown]
 # ## `If` Statement in Python
@@ -86,19 +108,43 @@ print(motto)
 # | `A` is **greater or equal** to `B` | >= | `A` >= `B` |
 # | `A` is **less** to `B` | < | `A` < `B` |
 # | `A` is **less or equal** to `B` | <= | `A` <= `B` |
-#
-# example of using `if` statement:
-#
-# if `price  == 0.0`, we appended the string `free` in the list
-# * `if price == 0.0:`
-#       `app.append("free")`
-#
-#
-# the code within the body of an `elif` clause is executed *only* if:
-# * The proceding `if` statement ( or the other proceding `elif` clauses) resolves to `False`; **and**
-# * The condition specified after the `elif` keyword evaluates to `True`.
-#
-# It is also possible the replace the last `elif` clause wiht an `else` clause
+
+#%%
+print("game" == "music")
+print("game" != "music")
+
+#%%
+if True:
+    print(1)
+
+#%%
+if True:
+    print(2)
+    print(3)
+
+#%%
+if 3 > 1 and "data" == 'data':
+    print("Both conditions are true!")
+
+#%%
+if 10 < 20 or 4 <= 5:
+    print("At least one condition is true")
+
+#%%
+if (20 > 3 and 2 != 1) or "Games" == "Games":
+    print("At least one condition is true")
+
+#%%
+if False:
+    print(1)
+else:
+    print("The condition above was false")
+
+#%%
+if False:
+    print(1)
+elif 30 > 5:
+    print("The condition above was false")
 
 #%%
 print(True and True)
@@ -114,24 +160,38 @@ print(False or False)
 
 #%% [markdown]
 # ## `Dictionary` in the Python
-# To create the dictionary, we use `{}`:
-# * Mapped each content rating to its corresponding number by following an `index:value` pattern. For instance, to map a rating of `'4+'` to the number 4,433, we type `'4'+:4433`
-# * Typed the entire sequence of `index:value` pairs, and separated each with a comma: `{'4+':4433, '9+':987, '12+':1155, '17+':662}`
-# * To get the value of the dictionary, we type `data_set['4+']`
-#
-#
-# To create the dictionary via adding one by one, we:
-# * We create an empty dictionary (`{}`) or list (`()`)
-# * We add values using the `dictionary_name[index] = value` technique (or the `list_name.append()` command in case of a list)
-#
-#
 # The index of a dictionary value is called a **Key**, then a whole `'4+':4433` is a **key-value pair**
 #
-# Dictionary key can be of almost any data type, but lists and dictionaries.
+# **Dictionary key** can be of almost any data type, but lists and dictionaries.
 #
-# Dictionary value can be of any data type: strings, integers, floats, booleans, lists, and dictionaries.
+# **Dictionary value** can be of any data type: strings, integers, floats, booleans, lists, and dictionaries.
 #
 # We use the `in` **operator** to check whether a key exists in a dictionary.
+
+#%%
+# Frist Way
+dictionary1 = {"key_1":1,"key_2":2}
+dictionary1["key_1"] #retrieve individual dictionary value
+
+#%%
+# Second Way
+dictionary2 = {}
+dictionary2["key_3"] = 100
+dictionary2["key_4"] = 200
+dictionary2["key_4"]
+
+#%%
+# Check whether a certain value exisit in the dictionary as a key
+
+"key_1" in dictionary2 # output False
+"key_3" in dictionary2 # output True
+100 in dictionary2 # output False (we can't check value)
+
+#%%
+# Updating dictionary value
+
+dictionary3 = {"key_1":100,"key_2":200}
+dictionary3["key_1"] += 600 # This will change the value to 700
 
 # %% [markdown]
 # To find out the minimum and the maximum values of a column, we can use the `min()` and the `max()` command.
@@ -151,17 +211,24 @@ print(sum_1)
 #%% [markdown]
 # ## Creating a function
 
-def square(a_number):
+#%%
+def square1(a_number):
     squared_number = a_number * a_number
     return squared_number
 
-def square(a_number):
+#%%
+def square2(a_number):
     return a_number * a_number
 
+square2(a_number=6) #a_number is **parameter**; 6 is **argument**
+
+#%%
 def add(a, b):
     a_sum = a + b
     return a_sum
+add(3,6)
 
+#%%
 def freq_table(index, data):
     frequency_table = {}
     for row in data[1:]:
@@ -172,6 +239,7 @@ def freq_table(index, data):
             frequency_table[column] = 1
     return frequency_table
 
+#%%
 def sum_or_difference1(a,b):
     a_sum = a + b
     difference = a - b
@@ -224,12 +292,12 @@ first_element, second_element = a_list
 a_sum = 1000
 length = 50
 
-def divide():
+def divide1():
     print(a_sum)
     print(length)
     return a_sum / length
 
-result = divide()
+result = divide1()
 print(result)
 
 #%% [markdown]
@@ -239,12 +307,12 @@ print(result)
 a_sum = 1000
 length = 50
 
-def divide():
+def divide2():
     a_sum = 15
     length = 3
     print(a_sum)
     print(length)
     return a_sum / length
 
-result = divide()
+result = divide2()
 print(result)
